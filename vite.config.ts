401a4c53c5a88df8ea5d1cbfb175f1c38a436d67
samiboutgoulla-smb <samiboutgoulla931@gmail.com>
2026-06-07 14:44:08 +0100

    vite.config update

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/prisma-color-website/', // Ligne cruciale pour corriger l'erreur 404 sur GitHub Pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
